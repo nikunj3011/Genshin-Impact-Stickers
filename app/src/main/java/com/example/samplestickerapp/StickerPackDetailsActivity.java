608a2b91bbff009598388c85f835d68b10383184
Nikunj Rathod <nikunj.rathod@ditechtesting.com>
2023-08-20 13:27:22 -0300

    new

--- app/src/main/java/com/example/samplestickerapp/StickerPackDetailsActivity.java
+++ app/src/main/java/com/example/samplestickerapp/StickerPackDetailsActivity.java
@@ -104,7 +104,7 @@ public class StickerPackDetailsActivity extends BaseActivity {
         });
         AdRequest adRequest = new AdRequest.Builder().build();
 
-        InterstitialAd.load(this,"ca-app-pub-8299128249632072/1648534068", adRequest,
+        InterstitialAd.load(this,"ca-app-pub-8299128249632072/3094607940", adRequest,
                 new InterstitialAdLoadCallback() {
                     @Override
                     public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
